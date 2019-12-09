3a6f9fb0ff66b6b9f2a530444d819987dd5049ee
Ubuntu <ubuntu@ip-172-31-41-20.ec2.internal>
2019-12-09 10:08:42 +0000

    Done

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,19 +1,15 @@
+import { Component, Input } from '@angular/core';
 import { RouterService } from '../services/router.service';
 import { Note } from '../note';
-import { Component, OnInit, Input } from '@angular/core';
-
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent {
-  @Input()
-  note: Note;
-  constructor(private router: RouterService) {
-  }
-  editNote() {
-   this.router.routeToEditNoteView(this.note.id);
-   console.log('editNote() from Note.. :', this.note);
-  }
+  @Input() note: Note;
+  constructor(private routerService: RouterService) { }
+  openEditView() {
+    this.routerService.routeToEditNoteView(this.note.id);
+   }
 }
